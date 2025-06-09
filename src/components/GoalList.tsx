4e951b81b2ae3b6fc8c92d8b24a40803a511c703
jakkaphatCM <jakkaphat.m@pdkm.tech>
2025-06-09 17:33:08 +0700

    feat : info-box :: add info-box

--- src/components/GoalList.tsx
+++ src/components/GoalList.tsx
@@ -1,5 +1,7 @@
+import type { ReactNode } from "react";
 import type { Goal } from "../App";
 import GoalItem from "./GoalItem";
+import InfoBox from "./InfoBox";
 
 type Props = {
   goals: Goal[];
@@ -7,15 +9,33 @@ type Props = {
 };
 
 export default function GoalList({ goals, onDelete }: Props) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">You have no goals yet. Start adding some!</InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="high">
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <GoalItem id={goal.id} title={goal.title} onDelete={onDelete}>
-            <p>{goal.description}</p>
-          </GoalItem>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <GoalItem id={goal.id} title={goal.title} onDelete={onDelete}>
+              <p>{goal.description}</p>
+            </GoalItem>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
